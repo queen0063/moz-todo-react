275c089dbe95cf080224f9d4b8cb377068acaf2b
queen00 <3286756348@qq.com>
2025-04-13 20:10:14 +0800

    finish deleting tasks and sycing completed status

--- src/App.js
+++ src/App.js
@@ -13,6 +13,23 @@ export default function App(props) {
 
     setTasks([...tasks, newTask]);
   }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
   
   
   const taskList = tasks.map((task) => (
@@ -21,8 +38,19 @@ export default function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
     />
   ));
+  
+  
+  
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  
+
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -33,7 +61,7 @@ export default function App(props) {
         <FilterButton filterName="Active" />
         <FilterButton filterName="Completed" />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
